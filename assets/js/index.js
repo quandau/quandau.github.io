ee10210f6c5bcf7fc041fd02d9fccf76a3bce3e1
infohquan <56305767+infohquan@users.noreply.github.com>
2020-05-15 18:41:34 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,106 @@
+$(function() {
+    "use strict";
+
+     // chart 1
+	 
+		  var ctx = document.getElementById('chart1').getContext('2d');
+		
+			var myChart = new Chart(ctx, {
+				type: 'line',
+				data: {
+					labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+					datasets: [{
+						label: 'Publication',
+						data: [1, 2, 2, 3, 2, 4, 5, 6, 3, 1, 1],
+						backgroundColor: '#fff',
+						borderColor: "transparent",
+						pointRadius :"0",
+						borderWidth: 2
+					}, {
+						label: 'Methodology',
+						data: [2, 2, 4, 5, 8, 9, 12, 9, 8, 4],
+						backgroundColor: "rgba(255, 255, 255, 0.25)",
+						borderColor: "transparent",
+						pointRadius :"0",
+						borderWidth: 1
+					}]
+				},
+			options: {
+				maintainAspectRatio: false,
+				legend: {
+				  display: false,
+				  labels: {
+					fontColor: '#ddd',  
+					boxWidth:40
+				  }
+				},
+				tooltips: {
+				  displayColors:false
+				},	
+			  scales: {
+				  xAxes: [{
+					ticks: {
+						beginAtZero:true,
+						fontColor: '#ddd'
+					},
+					gridLines: {
+					  display: true ,
+					  color: "rgba(221, 221, 221, 0.08)"
+					},
+				  }],
+				   yAxes: [{
+					ticks: {
+						beginAtZero:true,
+						fontColor: '#ddd'
+					},
+					gridLines: {
+					  display: true ,
+					  color: "rgba(221, 221, 221, 0.08)"
+					},
+				  }]
+				 }
+
+			 }
+			});  
+		
+		
+    // chart 2
+
+		var ctx = document.getElementById("chart2").getContext('2d');
+			var myChart = new Chart(ctx, {
+				type: 'doughnut',
+				data: {
+					labels: ["Hydrology", "Climate Change", "Modelling", "Optimisation"],
+					datasets: [{
+						backgroundColor: [
+							"#ffffff",
+							"rgba(255, 255, 255, 0.70)",
+							"rgba(255, 255, 255, 0.50)",
+							"rgba(255, 255, 255, 0.20)"
+						],
+						data: [80, 75, 95, 35],
+						borderWidth: [0, 0, 0, 0]
+					}]
+				},
+			options: {
+				maintainAspectRatio: false,
+			   legend: {
+				 position :"bottom",	
+				 display: false,
+				    labels: {
+					  fontColor: '#ddd',  
+					  boxWidth:15
+				   }
+				}
+				,
+				tooltips: {
+				  displayColors:false
+				}
+			   }
+			});
+		
+
+		
+		
+   });	 
+   
